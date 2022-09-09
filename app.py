5fca84e422217d1397a5b96a6e1c2be3aa400ed9
TivonB <112200765+TivonB@users.noreply.github.com>
2022-09-08 22:29:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,35 +53,6 @@ with st.container():
     with F_right_column:
         st.image(img)
         
-"""# ---- MORE INFO ----
-with st.container():
-    st.write("---")
-    left_column, right_column = st.columns(2)
-    with left_column:
-        st.header("Further Left Testing")
-        st.write("##")
-        st.write("This is testing of the left column")
-    with right_column:
-        st.header("Further Right Testing")
-        st.write("##")
-        st.write("This is testing of the right column")
-        
-# ---- IMAGE ----
-with st.container():
-    st.write("---")
-    image_column, text_column = st.columns(2)
-    with image_column:
-        st.header("Further Image Testing")
-        st.image(img)
-    with text_column:
-        st.header("Further Image Text Testing")
-        st.write("##")
-        st.write("This is testing of the Image Text column")
-        
-# ---- SLIDER ---- 
-with st.container():
-    st.write("---")
-    st.slider("Slider tester", 1, 5000, 2000)
-    st.sidebar.slider("Control which features?",1,2000,100)"""
+
     
         
