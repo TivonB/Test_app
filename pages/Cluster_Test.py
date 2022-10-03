2a5dea7dfd9620fb0e39519a89378a37bbf0d1ab
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 17:08:59 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -12,3 +12,23 @@ for filename in glob.glob('Flowers/*.png'): #assuming gif
     im = im.resize(newsize)
     image_list.append(im)
 st.image(image_list)
+
+col, row = im.size
+count = 0
+for img in im:
+  Rvalue = 0
+  Gvalue = 0
+  Bvalue = 0
+  for x in range(col):
+    for y in range(row):
+      Rvalue+= int(img.getpixel((x,y))[0])
+      Gvalue+= int(img.getpixel((x,y))[1])
+      Bvalue+= int(img.getpixel((x,y))[2])
+  Rvalue /= (col*row)
+  Gvalue /= (col*row)
+  Bvalue /= (col*row)
+  st.write("Cluster: ", count) 
+  st.write("R Value: ", Rvalue)
+  st.write("G Value: ", Gvalue)
+  st.write("B Value: ", Bvalue)
+  count+=1
