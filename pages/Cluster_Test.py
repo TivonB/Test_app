28c81edae6db0e8eb3b5d158d28a76ef7f6f8a0b
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 16:52:20 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -8,6 +8,6 @@ st.set_page_config(page_title="Cluster", layout="wide")
 image_list = []
 for filename in glob.glob('Flowers/*.png'): #assuming gif
     im=Image.open(filename)
-    im = im.resize(200,200)
+    im = im.resize((200,200))
     image_list.append(im)
 st.image(image_list)
