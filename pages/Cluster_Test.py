7ef403e9106171cdc748c29f88f74486286b2bb6
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 16:47:50 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -1,8 +1,13 @@
 #For Cluster
 import streamlit as st
 from PIL import Image
-
+import glob
                    
 
 
 st.set_page_config(page_title="Cluster", layout="wide")
+image_list = []
+for filename in glob.glob('Flowers/*.png'): #assuming gif
+    im=Image.open(filename)
+    image_list.append(im)
+st.image(image_list)
