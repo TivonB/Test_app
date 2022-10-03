ba2c03e94157019d1f07031c3059b6cab4379b21
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 17:29:12 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -5,44 +5,57 @@ import glob
 
 
 st.set_page_config(page_title="Cluster", layout="wide")
-image_list = []
-newsize = (200,200)
-for filename in glob.glob('Flowers/*.png'): #assuming gif
-    im=Image.open(filename)
-    im = im.resize(newsize)
-    image_list.append(im)
-st.image(image_list)
+#image_list = []
+#newsize = (200,200)
+#for filename in glob.glob('Flowers/*.png'): #assuming gif
+#    im=Image.open(filename)
+#    im = im.resize(newsize)
+#    image_list.append(im)
+#st.image(image_list)
 
-count = 0
-for img in image_list:
-  temp_pixel = img.getpixel((0,0))
-  if type(temp_pixel) is tuple:
-       num = len(temp_pixel)
-  else:
-       num = 1
-  col, row = img.size
-  if num == 1:
-    value = 0
-    for x in range(col):
-      for y in range(row):
-        value+=int(img.getpixel((x,y)))
-    value /= (col*row)
-    st.write("Cluster: ", count) 
-    st.write("Value: ", value)
-  else:
-    Rvalue = 0
-    Gvalue = 0
-    Bvalue = 0
-    for x in range(col):
-      for y in range(row):
-        Rvalue+= int(img.getpixel((x,y))[0])
-        Gvalue+= int(img.getpixel((x,y))[1])
-        Bvalue+= int(img.getpixel((x,y))[2])
-    Rvalue /= (col*row)
-    Gvalue /= (col*row)
-    Bvalue /= (col*row)
-    st.write("Cluster: ", count) 
-    st.write("R Value: ", Rvalue)
-    st.write("G Value: ", Gvalue)
-    st.write("B Value: ", Bvalue)
-  count+=1
+#count = 0
+#for img in image_list:
+#  temp_pixel = img.getpixel((0,0))
+#  if type(temp_pixel) is tuple:
+#       num = len(temp_pixel)
+#  else:
+#       num = 1
+#  col, row = img.size
+#  if num == 1:
+#    value = 0
+#    for x in range(col):
+#      for y in range(row):
+#        value+=int(img.getpixel((x,y)))
+#    value /= (col*row)
+#    st.write("Cluster: ", count) 
+#    st.write("Value: ", value)
+#  else:
+#    Rvalue = 0
+#    Gvalue = 0
+#    Bvalue = 0
+#    for x in range(col):
+#      for y in range(row):
+#        Rvalue+= int(img.getpixel((x,y))[0])
+#        Gvalue+= int(img.getpixel((x,y))[1])
+#        Bvalue+= int(img.getpixel((x,y))[2])
+#    Rvalue /= (col*row)
+#    Gvalue /= (col*row)
+#    Bvalue /= (col*row)
+#    st.write("Cluster: ", count) 
+#    st.write("R Value: ", Rvalue)
+#    st.write("G Value: ", Gvalue)
+#    st.write("B Value: ", Bvalue)
+#  count+=1
+path = r"Flowers/"
+# change the working directory to the path where the images are located
+os.chdir(path)
+
+# this list holds all the image filename
+flowers = []
+with os.scandir(path) as files:
+  # loops through each file in the directory
+    for file in files:
+        if file.name.endswith('.png'):
+          # adds only the image files to the flowers list
+            flowers.append(file.name)
+st.write(flowers)
