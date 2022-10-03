590cee6aa03cd8ab565ae3b7f6244f29971d1c86
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 17:15:33 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -15,20 +15,34 @@ st.image(image_list)
 
 count = 0
 for img in image_list:
+  temp_pixel = img.getpixel((0,0))
+  if type(temp_pixel) is tuple:
+       num = len(temp_pixel)
+   else:
+       num = 1
   col, row = img.size
-  Rvalue = 0
-  Gvalue = 0
-  Bvalue = 0
-  for x in range(col):
-    for y in range(row):
-      Rvalue+= int(img.getpixel((x,y))[0])
-      Gvalue+= int(img.getpixel((x,y))[1])
-      Bvalue+= int(img.getpixel((x,y))[2])
-  Rvalue /= (col*row)
-  Gvalue /= (col*row)
-  Bvalue /= (col*row)
-  st.write("Cluster: ", count) 
-  st.write("R Value: ", Rvalue)
-  st.write("G Value: ", Gvalue)
-  st.write("B Value: ", Bvalue)
+  if num == 1:
+    value = 0
+    for x in range(col):
+      for y in range(row):
+        value+=int(img.getpixel((x,y)))
+    value /= (col*row)
+    st.write("Cluster: ", count) 
+    st.write("Value: ", value)
+  else:
+    Rvalue = 0
+    Gvalue = 0
+    Bvalue = 0
+    for x in range(col):
+      for y in range(row):
+        Rvalue+= int(img.getpixel((x,y))[0])
+        Gvalue+= int(img.getpixel((x,y))[1])
+        Bvalue+= int(img.getpixel((x,y))[2])
+    Rvalue /= (col*row)
+    Gvalue /= (col*row)
+    Bvalue /= (col*row)
+    st.write("Cluster: ", count) 
+    st.write("R Value: ", Rvalue)
+    st.write("G Value: ", Gvalue)
+    st.write("B Value: ", Bvalue)
   count+=1
