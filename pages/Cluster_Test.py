e2dce7ec1a3e6176bf5738bfe786aae1bf46cb07
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 21:05:21 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -26,13 +26,10 @@ from sklearn.decomposition import PCA
 
 # --- Cluster Test ----
 st.set_page_config(page_title="Cluster", layout="wide")
-fileName = "Flowers/flower_"
-fileTypeName = ".png"
 image_list = []
 newsize = (224,224)
-#for filename in glob.glob('Flowers/*.png'): #assuming gif
-for i in range(25):
-    im=Image.open(fileName+str(i)+fileTypeName)
+for filename in glob.glob('Flowers/*.png'): #assuming gif
+    im=Image.open(filename)
     im = im.convert('RGB')
     img = np.array(im)
     imgArr=img.reshape((img.shape[1]*img.shape[0],3))
@@ -40,7 +37,7 @@ for i in range(25):
     #im = im.resize(newsize)
     image_list.append(im)
 
-st.write(image_list)
+st.image(image_list)
     
 #st.image(image_list)
 st.write("ReShape: ", imgArr.shape)
