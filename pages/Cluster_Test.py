9964f588757c870e0d5dd70b24089d424539b87e
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-03 21:18:44 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -43,7 +43,7 @@ model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
 
 def extract_features(file, model):
     # load the image as a 224x224 array
-    img = image.open(file)
+    img = Image.open(file)
     # convert from 'PIL.Image.Image' to numpy array
     img = np.array(img) 
     # reshape the data for the model reshape(num_of_samples, dim 1, dim 2, channels)
