80f193a754b79895ba9ed2c988aca9b186698990
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-11 11:00:56 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -54,12 +54,12 @@ def image_feature(image_list):
     return features,img_name
 
 img_features,img_name = image_feature(image_list)
-#pca = PCA(n_components=100, random_state=22)
-#pca.fit(img_features)
-#x = pca.transform(img_features)
-k = 7
+pca = PCA(n_components=100, random_state=22)
+pca.fit(img_features)
+x = pca.transform(img_features)
+k = 5
 clusters = KMeans(k, random_state = 22)
-clusters.fit(img_features)
+clusters.fit(x)
 st.write(clusters.labels_)
 image_cluster = pd.DataFrame(img_name,columns=['image'])
 image_cluster["clusterid"] = clusters.labels_
