f0acddf85b3d219e94fcb9b782bc24a42994aa49
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-11 11:07:45 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -40,7 +40,7 @@ for filename in glob.glob('Weather/*.png'): #assuming gif
 
 def image_feature(image_list):
     model = InceptionV3(weights='imagenet', include_top=False)
-    #model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
+    model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
     features = [];
     img_name = [];
     for i in range(len(image_list)):
