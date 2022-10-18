c1100456fa73290e3416c89f4bfedad11a20dacc
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-17 23:56:57 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -11,7 +11,7 @@ import pickle
 
 # for loading/processing the images  
 from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.applications.Xception import Xception
+from tensorflow.keras.applications.xception import Xception
 from tensorflow.keras.applications.inception_v3 import preprocess_input
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import img_to_array
