d5bfa9089428d0a62db89a96c3d341bc54c03692
TivonB <112200765+TivonB@users.noreply.github.com>
2022-10-17 23:49:37 -0400

    Update Cluster_Test.py

--- pages/Cluster_Test.py
+++ pages/Cluster_Test.py
@@ -40,7 +40,7 @@ for filename in glob.glob('Weather/*.png'): #assuming gif
 
 def image_feature(image_list):
     model = VGG19()
-    model = Model(inputs = model.inputs, outputs = model.layers[11].output)
+    model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
     features = [];
     img_name = [];
     for i in range(len(image_list)):
